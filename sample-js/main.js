ddb6cc7feb28f72c696ad0da3be10bf109c05593
gawgaw <gawgaw.developer@gmail.com>
2020-09-26 13:21:47 +0900

    読み込み時と画面のリサイズから1秒後に処理を走らせるように変更

--- sample-js/main.js
+++ sample-js/main.js
@@ -1,12 +1,20 @@
 'use strict';
 
-// ヘッダーの高さを取得
-const height = document.getElementById('header').clientHeight;
-// 高さ文のマージンを適用する要素を取得
-const topImage = document.getElementById('top-image');
+function addMarginTop() {
+    // ヘッダーの高さを取得
+    const height = document.getElementById('header').clientHeight;
+    // 高さ文のマージンを適用する要素を取得
+    const topImage = document.getElementById('top-image');
+    setTimeout(() => {
+      topImage.style.marginTop = height + 'px';
+    }, 1000);
+  
+}
+
+addMarginTop();
 
 // ページ読み込み時にスタイルを追加
-window.addEventListener('load', () => {
-  topImage.style.marginTop = height + 'px';
+window.addEventListener('resize', () => {
+  addMarginTop();
 });
 
